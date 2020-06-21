b684320bf117e4cdc6bcff15910ef993fa82280f
advait <advait268742@gmail.com>
2020-06-21 18:25:14 +0530

    added files

---
+++ random_grid_generator.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 21 14:21:14 2020
+
+@author: advait
+"""
+
+
+from random import randint, shuffle
+from time import sleep
+
+grid = []
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+
+
+
+def checkGrid(grid):
+  for row in range(0,9):
+      for col in range(0,9):
+        if grid[row][col]==0:
+          return False
+
+  return True 
+
+#A backtracking/recursive function to check all possible combinations of numbers until a solution is found
+def solveGrid(grid):
+  global counter
+  #Find next empty cell
+  for i in range(0,81):
+    row=i//9
+    col=i%9
+    if grid[row][col]==0:
+      for value in range (1,10):
+        if not(value in grid[row]):
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:  
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:  
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:  
+                square=[grid[i][6:9] for i in range(6,9)]
+            #Check that this value has not already be used on this 3x3 square
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if checkGrid(grid):
+                counter+=1
+                break
+              else:
+                if solveGrid(grid):
+                  return True
+      break
+  grid[row][col]=0  
+
+numberList=[1,2,3,4,5,6,7,8,9]
+#shuffle(numberList)
+
+#A backtracking/recursive function to check all possible combinations of numbers until a solution is found
+def fillGrid(grid):
+  global counter
+  #Find next empty cell
+  for i in range(0,81):
+    row=i//9
+    col=i%9
+    if grid[row][col]==0:
+      shuffle(numberList)      
+      for value in numberList:
+        if not(value in grid[row]):
+          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
+            square=[]
+            if row<3:
+              if col<3:
+                square=[grid[i][0:3] for i in range(0,3)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(0,3)]
+              else:  
+                square=[grid[i][6:9] for i in range(0,3)]
+            elif row<6:
+              if col<3:
+                square=[grid[i][0:3] for i in range(3,6)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(3,6)]
+              else:  
+                square=[grid[i][6:9] for i in range(3,6)]
+            else:
+              if col<3:
+                square=[grid[i][0:3] for i in range(6,9)]
+              elif col<6:
+                square=[grid[i][3:6] for i in range(6,9)]
+              else:  
+                square=[grid[i][6:9] for i in range(6,9)]
+            if not value in (square[0] + square[1] + square[2]):
+              grid[row][col]=value
+              if checkGrid(grid):
+                return True
+              else:
+                if fillGrid(grid):
+                  return True
+      break
+  grid[row][col]=0             
+    
+fillGrid(grid)
+
+sleep(1)
+
+
+
+difficulty = -1
+while(True):
+    userinput = input("Set difficulty: \n E for Easy \n M for Moderate \n H for Hard \n").lower()
+    if(userinput == 'e'):
+        difficulty = 1
+        break
+    elif(userinput == 'm'):
+        difficulty = 3
+        break
+    elif(userinput == 'h'):
+        difficulty = 5
+        break
+    else:
+        print("not a valid input")
+
+attempts = difficulty
+counter=1
+while attempts>0:
+  row = randint(0,8)
+  col = randint(0,8)
+  while grid[row][col]==0:
+    row = randint(0,8)
+    col = randint(0,8)
+  backup = grid[row][col]
+  grid[row][col]=0
+  
+  copyGrid = []
+  for r in range(0,9):
+     copyGrid.append([])
+     for c in range(0,9):
+        copyGrid[r].append(grid[r][c])
+  
+  counter=0      
+  solveGrid(copyGrid)   
+  if counter!=1:
+    grid[row][col]=backup
+    attempts -= 1
+  
+
+#print(grid)
